fb6ef59172dc1fc761d32035888575142eae9160
yuguoneo <3551037616@qq.com>
2020-11-14 08:40:01 +0000

    delete file

--- Newgame.py
+++ Newgame.py
@@ -8,42 +8,50 @@ from random import randint as rd
 #这是一个数字游戏
 #this is a number game.
 
-print("数字炸弹\n
-      the number boom\n
-      我想一个数，在1到100间，你猜，我会告诉你大了还是小了\n
-      I think a number, between 1 and 100, you guessed it, I‘ll tell you whether it’s big or small\n")
+print('''      数字炸弹
+      the number boom
+      我想一个数，在1到100间，你猜，我会告诉你大了还是小了
+      I think a number, between 1 and 100, you guessed it, I‘ll tell you
+      it’s big or small''')
 
 playercoin = 200
 
 
-def guess(numberis):
+
+def guess(numberis, playercoin):
   guesssb = 1
   print("start guess!")
   while guesssb:
+    if playercoin <= 0:
+        break
     plgue = int(input("your guess:"))
-    playercoin = playercoin - 15
     if plgue == numberis:
       print("yes!it is!")
       playercoin = playercoin + 300
+      print("you win 300!")
       guesssb = 0
     elif plgue < numberis:
       print("the number is bigger.")
-    elif pigue > numberis:
+      playercoin = playercoin - 15
+    elif plgue > numberis:
       print("the number is smaller.")
-      
+      playercoin = playercoin - 15
 
+  return playercoin
+      
 while True:
-  if playercoin == 100000:
+  if playercoin == 1000:
     print("you are verry rich!")
     break
-  elif playcoin <= 0:
+  elif playercoin <= 0:
     print("gameover")
     break
   else:
     numis = rd(1,99)
-    guess(numis)
+    playercoin=guess(numis, playercoin)
+    print("you have %d now" % playercoin)
+    
     
     
 print("thanks for @indexofire's help.")
 
-      
