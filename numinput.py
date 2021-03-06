04b9f6329e551de0f295f0e422e199ba155192b4
yuguoneo <35537616@qq.com>
2021-03-06 16:09:18 +0800

    Create numinput.py
    new file

---
+++ numinput.py
@@ -0,0 +1,15 @@
+import random
+
+point = int("0")
+
+while True:
+    if point >= 50:
+        print("you win!")
+    a = random.randint(1, 9)
+    b = input("the number is ")
+    if a = b:
+        print("yes! it is!")
+        point = point + 1
+    else:
+        print("wrong!")
+    print("you have %d now" %point)
